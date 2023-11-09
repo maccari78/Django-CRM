e08e9d06908efa59afadc7d89b690932e4df17d5
Danilo Maccari <maccari78@gmail.com>
2023-11-09 18:01:30 -0300

    until DJANGO REGISTER USERS

--- website/views.py
+++ website/views.py
@@ -1,4 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 def home(request):
-    return render(request, 'home.html', {})
+    # Check to see if loggin in
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        # Authenticate
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You have been Logged In!")
+            return redirect('home')
+        else:
+            messages.success(request, "There was an ERROR Logging In, Please try again...")
+            return redirect('home')
+    else:
+        return render(request, 'home.html', {})
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "You have been Logged Out!")
+    return redirect('home')
